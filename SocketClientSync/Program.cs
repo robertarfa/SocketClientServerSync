7e2aec5d71ffb90b12cc7271d5f19222ae7d4311
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-08-27 19:12:45 -0300

    Adicionar arquivos de projeto.

---
+++ SocketClientSync/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClientSync
+{
+    internal class Program
+    {
+
+        public class SyncSocketClient
+        {
+            public static void StartClient()
+            {
+                byte[] buffer = new byte[1024];
+
+                try
+                {
+                    var hostName = Dns.GetHostName();
+                    IPHostEntry iPHostInfo = Dns.GetHostEntry(hostName);
+
+                    Console.WriteLine($"Host: {hostName}");
+
+                    IPAddress ipAddress = iPHostInfo.AddressList[0];
+
+                    IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 45500);
+
+                    Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    try
+                    {
+                        client.Connect(ipEndPoint);
+                        Console.WriteLine("Socket Connected");
+
+                        client.RemoteEndPoint.ToString();
+                        byte[] msg = Encoding.ASCII.GetBytes("This is just a test");
+                        int byteSent = client.Send(msg);
+                        int byteRec = client.Receive(buffer);
+
+                        Console.WriteLine($"Echoed test {Encoding.ASCII.GetString(buffer, 0, byteRec)}");
+
+                        //release the socket
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                    }
+                    catch (ArgumentNullException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                    catch (SocketException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press enter to continue....");
+            Console.ReadLine();
+
+
+            SyncSocketClient.StartClient();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketServerSync/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServerSync
+{
+    internal class Program
+    {
+
+        public class SyncServerSocket
+        {
+            public static string data = null;
+
+            public static void StartListener()
+            {
+                byte[] buffer = new byte[1024];
+
+                var hostName = Dns.GetHostName();
+                IPHostEntry iPHostEntry = Dns.GetHostEntry(hostName);
+                IPAddress ipAddress = iPHostEntry.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 45500);
+
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+                try
+                {
+
+                    listener.Bind(localEndPoint);
+                    listener.Listen(10);
+
+                    while (true)
+                    {
+                        Console.WriteLine($"Waiting for a connection...");
+
+                        Socket handler = listener.Accept();
+                        data = null;
+
+                        while (true)
+                        {
+                            int byteRec = handler.Receive(buffer);
+                            data += Encoding.ASCII.GetString(buffer, 0, byteRec);
+
+                            if (data.IndexOf("<EOF>") == -1)
+                            {
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine($"Text received: {data}");
+
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+                        handler.Send(msg);
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+
+
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Press any key to exit!");
+                Console.ReadLine();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to continue....");
+            Console.ReadLine();
+
+            SyncServerSocket.StartListener();
+            Console.ReadLine();
+        }
+    }
+}
